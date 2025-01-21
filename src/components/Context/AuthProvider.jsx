bf4a92e9679e82801b05f950ab3f606b860adb2d
rohanSnippet <rohan110620@gmail.com>
2025-01-21 17:10:22 +0530

    authprovider changed

--- src/components/Context/AuthProvider.jsx
+++ src/components/Context/AuthProvider.jsx
@@ -6,6 +6,7 @@ export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
   const [city, setCity] = useState();
+  const[session, setSession] = useState()
   const [userData, setUserData] = useState({
     username: "",
     token: "",
@@ -71,19 +72,51 @@ const AuthProvider = ({ children }) => {
 
   useEffect(() => {
    
-    const getSession = ()=>{
+     const getSession = ()=>{
       axios.get(`http://localhost:8082/auth/user-info`,{withCredentials:true}).then((res)=>{
         console.log(res.data)
+         localStorage.setItem("username",res.data.email)
+             setSession(res.data)
+                console.log(res.data)
+              setUserData(prevData=>({...prevData,username:res.data.email}))
+             
       }).catch(err=>{
         console.error(err)
       })
+    } 
+      
+  getSession();   
+
+   const handleOAuth2Response = async () => {
+    const response = await fetch('http://localhost:8082/auth/callback', {
+        method: 'GET',
+        credentials: 'include',
+    });
+
+    if (response.ok) {
+        const data = await response.json();
+        const token = data.token;
+    console.log("Got the token :",data)
+  
+      localStorage.setItem('access-token', token);
+      setUserData(prevData=>({...prevData,token:token}))
+     
+        
+       
+
+        //window.location.href = 'http://localhost:5173/';
+    } else {
+        console.error('Failed to authenticate ');
     }
+};
 
-  getSession();    
 
+handleOAuth2Response(); 
   }, []);
 
   const authInfo = {
+    session,
+    setSession,
     createUser,
     userData,
     signIn,
